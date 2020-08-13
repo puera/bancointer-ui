b25d18166cb1fec1d2338ce6ca00e83f4f3e3d00
Renann Souza <renann_puera@hotmail.com>
2020-08-13 20:21:42 -0300

    :art: feat: add styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { ThemeProvider } from 'styled-components';
+
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>Hello World</div>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 };
 
 export default App;
