fa27dec9b7b860119efb2602f4c1817e95700e9a
Renann Souza <renann_puera@hotmail.com>
2020-08-13 23:27:18 -0300

    ✨ feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import React, { memo } from 'react';
+
+import { Container, Wrapper, LeftNav, RightNav } from './styles';
+import { BancoInter } from '../../assets';
+import Button from '../Button';
+import AccountDropDown from '../AccountDropDown';
+
+interface User {
+  name: string;
+}
+
+const Header: React.FC = () => {
+  const user: User = { name: 'Renann Souza' };
+
+  return (
+    <Container>
+      <Wrapper>
+        <LeftNav>
+          <BancoInter />
+          Internet Banking
+        </LeftNav>
+        <RightNav>
+          <Button variant="secondary">Simulador Renda Fixa</Button>
+          <AccountDropDown name={user.name} />
+        </RightNav>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default memo(Header);
