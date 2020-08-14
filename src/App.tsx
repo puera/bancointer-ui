7473aca23e096a84e71699fdeac738f486960468
Renann Souza <renann_puera@hotmail.com>
2020-08-13 23:32:05 -0300

    ✨ feat: create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import Gradient from './components/Gradient';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div>Hello World</div>
+      <Header />
+      <Gradient />
       <GlobalStyles />
     </ThemeProvider>
   );
